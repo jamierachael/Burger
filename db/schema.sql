826148c07c959f6933f414a47ddc180afbc4596f
jamierachael <jamie.rachael@gmail.com>
2020-01-07 02:07:13 -0500

    removed nodemon

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,11 @@
 
 DROP DATABASE IF EXISTS burgers_db;
-
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
 
 CREATE TABLE burgers (
   id int AUTO_INCREMENT NOT NULL,
-  burger_name varchar(100) NOT NULL,
+  burger_name varchar(255) NOT NULL,
   devoured boolean,
   PRIMARY KEY(id)
 );
