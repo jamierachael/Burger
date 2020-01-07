fa449602f0c198cd93ec562f4f438d4de75c2cab
jamierachael <jamie.rachael@gmail.com>
2020-01-07 01:36:21 -0500

    Added JAWSDB connection pint

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,25 @@
 // Jamie Morris
 // Homework-11  Eat-Da-Burger!
-
+var connection;
 // Requires mysql
 var mysql = require("mysql");
 
 // Creates connection
-var connection = mysql.createConnection({
-  host: "localhost",
-  //Port
-  port: 3306,
-  // Username
-  user: "root",
-  // Password
-  password: "Cheeseme1",
-  // DataBase
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    //Port
+    port: 3306,
+    // Username
+    user: "root",
+    // Password
+    password: "Cheeseme1",
+    // DataBase
+    database: "burgers_db"
+  });
+};
 
 // Error handling
 connection.connect(function (err) {
